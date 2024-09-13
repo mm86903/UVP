62d8c16835afd2aeeb95735255b6ba2ef72fe5d1
Marija Mrvar <mm86903@student.uni-lj.si>
2024-09-13 14:43:49 +0200

    Testiranje gita

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import requests
 url = "https://okusno.je/recept/kremna-juha-iz-hokaido-buce-1"
 page = requests.get(url)
 
-#print(page.text)
 
 soup = BeautifulSoup(page.text, "html.parser")
 zgornja = soup.find("div", {"class":"flex justify-between items-center w-full p-16 md:p-0 md:mx-64 lg:mx-100 text-12 md:text-14 text-secondary dark:text-white"})
-#print(zgornja)
+
 
 kuhanje = zgornja.find_all("div", {"class":"flex flex-col text-center"})[1]
 print(kuhanje.text.split("\n")[1])
+
